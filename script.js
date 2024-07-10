2648fc01fb98ca23eb9099656878e6bcb26d623b
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-07-10 17:28:49 +0530

    Latest event section under build

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 let count = document.querySelectorAll('.count');
-let update = 1000;
-async function increment(id,value,add){
-for(let i=1000;i<=value;i+=update){
+
+async function increment(id,value,add,sleepTime,update=1000){
+for(let i=update;i<=value;i+=update){
     count[id].textContent = i+add;
-    await sleep(20);
+    await sleep(sleepTime);
 }
 console.log('loop terminated');
 }
-increment(0,89052,52);
-increment(1,12199,199);
-increment(2,833000,110);
-increment(3,2509,509);
+increment(0,89052,52,60);
+increment(1,12199,199,500);
+increment(2,833000,110,1);
+increment(3,2509,509,500,200);
