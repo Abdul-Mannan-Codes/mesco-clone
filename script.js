8bb589c4f685c209f53061783c6b4c6acd56a58b
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-07-11 16:46:24 +0530

    sticky position done

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ increment(0,89052,52,60);
 increment(1,12199,199,500);
 increment(2,833000,110,1);
 increment(3,2509,509,500,200);
+
+window.addEventListener('scroll', ()=> {
+    let el = document.querySelector('.go-up');
+    let pos = 500;
+    if(window.scrollY >= pos){
+        el.classList.add('sticky');
+    }
+    else{
+        el.classList.remove('sticky');
+    }
+})
+window.addEventListener('scroll', ()=> {
+    let el = document.querySelector('.header-div');
+    let pos = 180;
+    if(window.scrollY >= pos){
+        el.classList.add('margin-0', 'sticky');
+    }
+    else{
+        el.classList.remove('margin-0', 'sticky');
+    }
+})
